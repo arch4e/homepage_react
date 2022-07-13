1b587cb9142d01aab73f093631f3e5d0fcd02f50
0x04 <0x04@example.com>
2022-07-13 22:43:55 +0900

    feat: router and some pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,51 @@
-import '../styles/global.css';
+import { Link, Outlet, Route, Routes } from "react-router-dom";
+
+/* components */
+import About   from "./About";
+import Gallery from "./Gallery";
+
+/* Stylesheets */
+import "../styles/global.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index          element={ <About />   } />
+          <Route path="gallery" element={ <Gallery /> } />
+        </Route>
+        <Route path="*" element={ <NotFound /> } />
+      </Routes>
+    </>
+  )
+}
+
+function Layout() {
+  return(
+    <>
+      <ul id="nav">
+        <li>
+          <Link to="/">about</Link>
+          <Link to="/gallery">gallery</Link>
+        </li>
+      </ul>
+
+      <div id="main">
+        <Outlet />
+      </div>
+
+      <div id="footer">
+        <p>&copy; 2022 arch4e.com all rights reserved.</p>
+      </div>
+    </>
+  );
+}
+
+function NotFound() {
+  return (
+    <div id="error-404">
+      <span className="accent-color">404</span><span>Not Found.</span>
     </div>
   );
 }
