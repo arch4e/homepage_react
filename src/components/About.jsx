d6c0fa787c4e0c9cdd2d9115882a7f2d2dcf6af1
0x04 <0x04@arch4e.com>
2022-10-29 20:14:35 +0900

    change: edit profile name and sns account uri

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -23,12 +23,12 @@ function About() {
       <div className="profile">
         <img className="icon" src={ icon } alt="icon" />
         <div className="description">
-          <p className="name">0x04</p>
+          <p className="name">ShioN</p>
           <p className="skill">code writer: Elixir, Node, Rust, ... ( 2014~ ) /</p>
           <p className="skill">3DCG modeler ( 2021~ )</p>
           <div className="links">
-            <a href="https://twitter.com/NoIV_Dev"><img src={ twitter} alt="twitter" /></a>
-            <a href="https://github.com/LyIV"><img src={ github } alt="github" /></a>
+            <a href="https://twitter.com/arch4e_ShioN"><img src={ twitter} alt="twitter" /></a>
+            <a href="https://github.com/arch4e"><img src={ github } alt="github" /></a>
             <a href="https://www.pixiv.net/users/20493799"><img src={ pixiv } alt="pixiv" /></a>
           </div>
         </div>
